3d7e2ff0662a832f9af0c21d11df7648cfaeedcb
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-13 22:15:22 +0000

    AdventOfCode: Day 18 [BROKEN second attempt; I thought it would blow the stack, it didn't but answer is wrong]

--- src/main/scala/io/github/teonistor/adventofcode/_18.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_18.scala
@@ -6,40 +6,59 @@ object _18 {
 
   private val lex = "(\\d+)|([+*])|(\\()|(\\))".r
 
-  private def parseLineRec(current: Int, remaining: Iterator[Match]): Int = {
+  private def explicitateMatch(next: Match) =
+    (next.group(1), next.group(2), next.group(3), next.group(4)) match {
+      case (v, null, null, null) => (1, v)
+      case (null, v, null, null) => (2, v)
+      case (null, null, v, null) => (3, v)
+      case (null, null, null, v) => (4, v)
+    }
+
+  private def computeIf(lhs: Int, op: Char, rhs: Int) =
+    op match {
+      case '+' => lhs + rhs
+      case '*' => lhs * rhs
+      case _ => rhs
+    }
+
+  private def parseLineRec(number: Int, op: Char, remaining: Iterator[Match]): Int = {
     if (remaining.isEmpty) {
-      current
+      number
+
     } else {
+      val next = explicitateMatch(remaining.next())
 
-      val signOrParen = remaining.next()
-      if (signOrParen.group(4) != null) {
-        current
+      next match {
+        case (1, newNumber) => parseLineRec(computeIf(number, op, newNumber.toInt), op, remaining)
+        case (2, newOp) => parseLineRec(number, newOp(0), remaining)
+        case (3, _) => parseLineRec(computeIf(number, op, parseLineRec(0, '.', remaining)), op, remaining)
+        case (4, _) => number
 
-      } else {
-        val numberOrParen = remaining.next()
-        val number =
-          if (numberOrParen.group(1) != null)
-            numberOrParen.group(1).toInt
-          else
-            parseLineFragment(remaining)
-
-        parseLineRec(if (signOrParen.group(2).equals("+")) current + number else current * number, remaining)
       }
     }
   }
 
-  private def parseLineFragment(remaining: Iterator[Match]): Int = {
-    val str = remaining.next().group(1)
-    if (str != null)
-      parseLineRec(str.toInt, remaining)
+  // 1568258264 -> too low
 
-    // To remove leading open paren. Dirty, I know, because we technically therefore have unbalanced paren
-    else
-      parseLineFragment(remaining)
-  }
+  // 2046446784 -> 53586054336
+  // 53107865816 -> too low (handcranked)
+
+
+  // handcrank debug data
+  // (4 + 6 * 8 + 6) + (4 * 6 + 9) * (5 + 9 * (9 + 7 + 2 + 7) + 6 * 4 * 6) * 6 * (2 * 8 * (2 + 6 * 9 + 5 + 9 + 5) + 8 * 6) + ((7 * 3 * 4 + 2 + 4) + (4 + 5 + 2 * 9 + 3)) = 2046446784 should be 53586054336
+
+//  private def parseLineFragment(remaining: Iterator[Match]): Int = {
+//    val str = remaining.next().group(1)
+//    if (str != null)
+//      parseLineRec(str.toInt, remaining)
+//
+//    // To remove leading open paren. Dirty, I know, because we technically therefore have unbalanced paren
+//    else
+//    parseLineFragment(remaining)
+//  }
 
   private def parseLine(line: String) = {
-    val dbg = parseLineFragment(lex.findAllMatchIn(line))
+    val dbg = parseLineRec(0, '.', lex.findAllMatchIn(line))
     println(s"$line = $dbg")
     dbg
   }
