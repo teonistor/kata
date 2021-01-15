de0862e1a3b14046d1782b90de440e27bc5ee0c8
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-15 23:24:57 +0000

    AdventOfCode: Day 18 third attempt, solved part 1 but doesn't scale to part 2

--- src/main/scala/io/github/teonistor/adventofcode/_18.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_18.scala
@@ -1,79 +1,116 @@
 package io.github.teonistor.adventofcode
 
+import scala.collection.LinearSeq
 import scala.util.matching.Regex.Match
 
 object _18 {
 
   private val lex = "(\\d+)|([+*])|(\\()|(\\))".r
 
-  private def explicitateMatch(next: Match) =
-    (next.group(1), next.group(2), next.group(3), next.group(4)) match {
+  private def tokenise(mtch: Match) =
+    (mtch.group(1), mtch.group(2), mtch.group(3), mtch.group(4)) match {
       case (v, null, null, null) => (1, v)
       case (null, v, null, null) => (2, v)
       case (null, null, v, null) => (3, v)
       case (null, null, null, v) => (4, v)
     }
 
-  private def computeIf(lhs: Int, op: Char, rhs: Int) =
+  private def computeIf(lhs: Long, op: Char, rhs: Long) =
     op match {
       case '+' => lhs + rhs
       case '*' => lhs * rhs
       case _ => rhs
     }
 
-  private def parseLineRec(number: Int, op: Char, remaining: Iterator[Match]): Int = {
+  private def parseLineRec(number: Long, op: Char, remaining: LinearSeq[Match]): (Long, LinearSeq[Match]) = {
     if (remaining.isEmpty) {
-      number
+      (number, remaining)
 
     } else {
-      val next = explicitateMatch(remaining.next())
+      val next = tokenise(remaining.head)
 
       next match {
-        case (1, newNumber) => parseLineRec(computeIf(number, op, newNumber.toInt), op, remaining)
-        case (2, newOp) => parseLineRec(number, newOp(0), remaining)
-        case (3, _) => parseLineRec(computeIf(number, op, parseLineRec(0, '.', remaining)), op, remaining)
-        case (4, _) => number
+        case (1, newNumber) => parseLineRec(computeIf(number, op, newNumber.toLong), op, remaining.tail)
+        case (2, newOp) => parseLineRec(number, newOp(0), remaining.tail)
+        case (3, _) =>
+          val parenResult = parseLineLaunch(remaining.tail)
+          parseLineRec(computeIf(number, op, parenResult._1), op, parenResult._2)
+        case (4, _) => (number, remaining.tail)
 
       }
     }
   }
 
-  // 1568258264 -> too low
-
-  // 2046446784 -> 53586054336
-  // 53107865816 -> too low (handcranked)
-
-
-  // handcrank debug data
-  // (4 + 6 * 8 + 6) + (4 * 6 + 9) * (5 + 9 * (9 + 7 + 2 + 7) + 6 * 4 * 6) * 6 * (2 * 8 * (2 + 6 * 9 + 5 + 9 + 5) + 8 * 6) + ((7 * 3 * 4 + 2 + 4) + (4 + 5 + 2 * 9 + 3)) = 2046446784 should be 53586054336
-
-//  private def parseLineFragment(remaining: Iterator[Match]): Int = {
-//    val str = remaining.next().group(1)
-//    if (str != null)
-//      parseLineRec(str.toInt, remaining)
-//
-//    // To remove leading open paren. Dirty, I know, because we technically therefore have unbalanced paren
-//    else
-//    parseLineFragment(remaining)
-//  }
-
-  private def parseLine(line: String) = {
-    val dbg = parseLineRec(0, '.', lex.findAllMatchIn(line))
-    println(s"$line = $dbg")
-    dbg
+  private def parseLineLaunch(remaining: LinearSeq[Match]) = {
+    val result = parseLineRec(0, '.', remaining)
+    println(s"$remaining => $result")
+    result
   }
 
-  def _1(input: String): Int = {
+  private def parseLine(line: String) = {
+    val dbg = parseLineLaunch(lex.findAllMatchIn(line).to(List))
+    println(s"$line = $dbg")
+    dbg._1
+  }
+
+  private def computeIf(val1: Long, op1: Char, val2: Long, op2: Char, val3: Long): (Long, Char, Long)  = {
+    val vvv = (op1, op2) match {
+      case ('+', _) => (val1 + val2, op2, val3)
+      case (_, '+') => (val1, op1, val2 + val3)
+      case ('*', _) => (val1 * val2, op2, val3)
+      case (_, '*') => (val1, op1, val2 * val3)
+      case _ => /* I should know which one it is :| */ (LazyList(val1, val2, val3).find(_ != -1L).get, '.', -1L)
+    }
+    println(s"vvv ($val1 $op1 $val2 $op2 $val3) => $vvv")
+    vvv
+  }
+
+  private def parseLineRec5(val1: Long, op1: Char, val2: Long, op2: Char, remaining: LinearSeq[Match]): (Long, LinearSeq[Match]) = {
+    if (remaining.isEmpty) {
+      (computeIf(val1, op1, val2, '.', -1)._1, remaining)
+
+    } else {
+      val next = tokenise(remaining.head)
+
+      next match {
+        case (1, newNumber) =>
+          val (newVal1, newOp1, newVal2) = if (val2 == -1) computeIf(val1, op1, newNumber.toLong, op2, -1)
+                                           else computeIf(val1, op1, val2, op2, newNumber.toLong)
+          parseLineRec5(newVal1, newOp1, newVal2, op2, remaining.tail)
+
+        case (2, newOp) => if (op1 == '.') parseLineRec5(val1, newOp(0), val2, op2, remaining.tail)
+                           else parseLineRec5(val1, op1, val2, newOp(0), remaining.tail)
+        case (3, _) =>
+          val parenResult = parseLineLaunch5(remaining.tail)
+          val (newVal1, newOp1, newVal2) = computeIf(val1, op1, val2, op2, parenResult._1)
+          parseLineRec5(newVal1, newOp1, newVal2, op2, parenResult._2)
+
+        case (4, _) => (computeIf(val1, op1, val2, '.', -1)._1, remaining.tail)
+      }
+    }
+  }
+
+  private def parseLineLaunch5(remaining: LinearSeq[Match]) = {
+    val result = parseLineRec5(-1, '.', -1, '.', remaining)
+    println(s"$remaining => $result")
+    result
+  }
+
+  private def parseLine5(line: String) = {
+    val dbg = parseLineLaunch5(lex.findAllMatchIn(line).to(List))
+    println(s"$line = $dbg")
+    dbg._1
+  }
+
+  def _1(input: String): Long = {
     input.split('\n').to(LazyList)
       .map(parseLine)
       .sum
   }
 
-  def _2(input: String): Int = {
-    0
+  def _2(input: String): Long = {
+    input.split('\n').to(LazyList)
+      .map(parseLine5)
+      .sum
   }
 }
-
-
-
-
