e7f3e7f54c87f1d65a5c1f8e9ff456a53f6dd404
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-20 19:28:58 +0000

    AdventOfCode: Day 19 part 2
    Analysis from before I discovered the mistake in parseNode() (with which I'm surprised everything but the last bit worked!'):
    Answer given => 370 too high
    388 if we don't enforce i in <42><31> but the example still passes
    With the <8> override only: example => 12, problem => 337
    With the <11> override only: example => 6, problem => 287

--- src/main/scala/io/github/teonistor/adventofcode/_19.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_19.scala
@@ -9,13 +9,12 @@ object _19 {
   private type Node = (Int, Seq[Seq[Int]])
   private type Leaf = (Int, String)
 
-  private def parseNode(num: String, alt1: String, alt2: String) = {
-    (num.toInt, LazyList(
-      alt1.split(' ').to(List).map(_.toInt),
-      if (alt2 == null) Seq.empty else alt2.split(' ').to(List).map(_.toInt)))
-  }
+  private def parseNode(num: String, alt1: String, alt2: String) = (
+    num.toInt,
+    if (alt2 == null) LazyList(alt1.split(' ').to(List).map(_.toInt))
+    else LazyList(alt1.split(' ').to(List).map(_.toInt), alt2.split(' ').to(List).map(_.toInt)))
 
-  private def parseRules(nodes: Seq[Node], leaves: Seq[Leaf], remaining: Seq[String]): (Seq[Node], Seq[Leaf]) = {
+  private def parseRules(nodes: Seq[Node], leaves: Seq[Leaf], remaining: Seq[String]): (Seq[Node], Seq[Leaf]) =
     if (remaining.isEmpty)
       (nodes, leaves)
     else
@@ -23,33 +22,44 @@ object _19 {
         case node(num, alt1, _, alt2) => parseRules(nodes.prepended(parseNode(num, alt1, alt2)), leaves, remaining.tail)
         case leaf(num, value) => parseRules(nodes, leaves.prepended((num.toInt, value)), remaining.tail)
       }
-  }
 
-  private def makeRegex(index: Int, nodes: Map[Int, Seq[Seq[Int]]], leaves: Map[Int, String]) = {
-    if (nodes contains index)
-      nodes(index).map(_.map(n => s"<$n>").prepended("").reduce(_ + _))
-        .to(LazyList).reduce(_ + "|" + _)
+  private def makeRegex(index: Int, nodes: Map[Int, Seq[Seq[Int]]], leaves: Map[Int, String], overrides: Map[Int, String]) =
+    if (overrides contains index)
+      overrides(index)
+    else if (nodes contains index)
+      "(" +
+        nodes(index).map(_.map(n => s"<$n>").mkString).to(LazyList).mkString("|") +
+      ")"
     else
       leaves(index)
-  }
 
-  private def makeRegexRec(str: String, nodes: Map[Int, Seq[Seq[Int]]], leaves: Map[Int, String]):String = {
+  private def makeRegexRec(str: String, nodes: Map[Int, Seq[Seq[Int]]], leaves: Map[Int, String], overrides: Map[Int, String]): String =
     if (inner.findAllMatchIn(str).isEmpty)
       str
     else
-      makeRegexRec(inner.replaceAllIn(str, mtch => "(" + makeRegex(mtch.group(1).toInt, nodes, leaves) + ")"), nodes, leaves)
-  }
+      makeRegexRec(inner.replaceAllIn(str, mtch => makeRegex(mtch.group(1).toInt, nodes, leaves, overrides)), nodes, leaves, overrides)
 
-  def _1(input: String): Int = {
+  private def solve(input: String, overrides: Map[Int, String]) = {
     val strings = input.split("\n\n")
     val (nodes, leaves) = parseRules(List.empty, List.empty, strings(0).split('\n'))
 
-    val rx = makeRegexRec("<0>", nodes.toMap, leaves.toMap).r
-
+    val rx = makeRegexRec("<0>", nodes.toMap, leaves.toMap, overrides).r
     strings(1).split('\n').count(rx.matches)
   }
 
+  def _1(input: String): Int = {
+    solve(input, Map.empty)
+  }
+
   def _2(input: String): Int = {
-    0
+    // We create overrides for rules 8 and 11 (now recursive):
+    //   8: 42 | 42 8
+    //  11: 42 31 | 42 11 31
+
+    val overrides = Map((8, "(<42>+)"), (11,
+      // Brute-force a regex that means <42> at least once then <31> the same number of times
+      (2 to 10).map(i => s"<42>{$i}<31>{$i}").mkString("(<42><31>|", "|", ")")))
+
+    solve(input, overrides)
   }
 }
