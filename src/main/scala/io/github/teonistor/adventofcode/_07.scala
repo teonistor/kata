f7cfc2df105d0b677d1ff8dbce3dbd98500dba66
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-21 23:09:54 +0000

    AdventOfCode: Day 7

--- src/main/scala/io/github/teonistor/adventofcode/_07.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_07.scala
@@ -2,42 +2,45 @@ package io.github.teonistor.adventofcode
 
 object _07 {
 
-  private type BagSpec = (String, List[(Int, String)])
+  private type Bag = (String, List[(Int, String)])
   private val innerBags = "(\\d+) (.+?) bags?"
   private val zeroBags = "(.+) bags contain no other bags\\.".r
   private val someBags = s"(.+) bags contain (($innerBags, )*$innerBags)\\.".r
 
-  private def digList(bags: String): List[(Int, String)] =
+  private def parseSomeBags(bags: String): List[(Int, String)] =
     innerBags.r.findAllMatchIn(bags)
       .map(mtch => (mtch.group(1).toInt, mtch.group(2)))
       .toList
 
-  private def digBags(rule: String): BagSpec = rule match {
-    case zeroBags(color) => new BagSpec(color, List.empty)
-    case someBags(color, bags, _*) => new BagSpec(color, digList(bags))
+  private def parseBag(bag: String): Bag = bag match {
+    case zeroBags(color) => new Bag(color, List.empty)
+    case someBags(color, bags, _*) => new Bag(color, parseSomeBags(bags))
   }
 
-  private def addIndirect(current: Map[String, Set[String]], prev: Map[String, Set[String]]): Map[String, Set[String]] = {
-    if (current.equals(prev)) {
-      return current
-    }
+  private def lookFor(color: String, bags: Map[String, Set[String]]): Set[String] =
+    if (bags contains color)
+      bags(color) ++ bags(color).flatMap(outerColor => lookFor(outerColor, bags))
+    else
+      Set.empty
 
-    val next = current.view.map(kv => (kv._1, kv._2 ++ kv._2.flatMap(current.getOrElse(_, Set.empty)))).toMap
-    addIndirect(next, current)
+  private def count(color: String, bags: Map[String, List[(Int, String)]]):Int = {
+    1 + bags(color).to(LazyList).map(countAndColor => countAndColor._1 * count(countAndColor._2, bags)).sum
   }
 
   def _1(input: String): Int = {
-    val bagSpecs = input.split('\n').map(digBags).to(List)
+    val bags = input.split('\n').map(parseBag).to(List)
 
-    val outerByInner = bagSpecs.to(Set).flatMap(spec => spec._2.map(subSpec => (subSpec._2, spec._1))).groupMap(_._1)(_._2)
-    val allOuterByInner = addIndirect(outerByInner, null)
+    val outerByInner = bags.to(Set).flatMap(bag => bag._2.map(subSpec => (subSpec._2, bag._1))).groupMap(_._1)(_._2)
+    println(outerByInner.keySet)
+    println(outerByInner)
 
-    // TODO Broken - passes example but the problem answer is wrong
-    println(allOuterByInner.keySet)
-    allOuterByInner.count(_._2.contains("shiny gold"))
+    lookFor("shiny gold", outerByInner).size
+
+
   }
 
   def _2(input: String): Int = {
-    0
+    val bags = input.split('\n').map(parseBag).to(List)
+    count("shiny gold", bags.toMap) - 1
   }
 }
