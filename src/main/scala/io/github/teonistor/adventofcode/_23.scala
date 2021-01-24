3fbe7a7b6c546a99ec58eefdd1762c0c8101be52
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-24 22:32:29 +0000

    AdventOfCode: Working on day 23 (commit this abomination because it contains progress)

--- src/main/scala/io/github/teonistor/adventofcode/_23.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_23.scala
@@ -1,26 +1,61 @@
 package io.github.teonistor.adventofcode
 
+import io.vavr.collection
+
+import scala.collection.immutable.TreeMap
+
+
 object _23 {
 
   def move(circle: List[Int]): List[Int] = {
+//    println(System.currentTimeMillis())
     val current = circle.head
     val take1 = circle(1)
     val take2 = circle(2)
     val take3 = circle(3)
 
+//    println(System.currentTimeMillis())
     val tail = circle.drop(4)
 
-    val destinationIndex = tail.zipWithIndex.filter(_._1 < current).maxByOption(_._1).getOrElse(tail.zipWithIndex.maxBy(_._1))
-      ._2 + 1
+//    println(System.currentTimeMillis())
 
-    tail.to(LazyList).take(destinationIndex).appended(take1).appended(take2).appended(take3)
+//    val tailWithIndex = tail.to(LazyList).zipWithIndex
+//    lazy val tuple = { println("Calculating!")
+//      tailWithIndex.maxBy(_._1) }
+
+//    println(System.currentTimeMillis())
+    // problem starts here
+//    val destinationIndex = tailWithIndex
+//      .filter(_._1 < current).maxByOption(_._1).getOrElse(tuple)
+//      ._2 + 1
+
+    // also a reverse map, but how??
+
+    val avoid = Set(current,take1,take2,take3)
+    def findNext (candidate:Int):Int={
+      println(candidate)
+      if (avoid contains candidate)
+        findNext(if (candidate == 1) 9 else candidate - 1)
+      else candidate
+    }
+
+    val destinationIndex = tail.indexOf(findNext(current)) + 1
+
+    // until here
+
+//    println(System.currentTimeMillis())
+    val result = tail.to(LazyList).take(destinationIndex).appended(take1).appended(take2).appended(take3)
       .concat(tail.drop(destinationIndex)).appended(current).to(List)
+
+//    println(System.currentTimeMillis())
+//    System.exit(-1)
+    result
   }
 
   def moveRec(circle: List[Int], movesLeft: Int): List[Int] =
     if (movesLeft == 0) circle
     else {
-      if (movesLeft % 1000 == 0) println(s"$movesLeft moves left")
+      /*if (movesLeft % 1000 == 0) */println(s"$movesLeft moves left")
       moveRec(move(circle), movesLeft - 1)
   }
 
