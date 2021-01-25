a19acf5528e04a17d6ea6cc6bb2d2a036f747d24
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-25 18:39:34 +0000

    AdventOfCode: Working on day 23 (this would finish part 2 in ~30 hours)

--- src/main/scala/io/github/teonistor/adventofcode/_23.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_23.scala
@@ -7,14 +7,14 @@ import scala.collection.immutable.TreeMap
 
 object _23 {
 
-  def move(circle: List[Int]): List[Int] = {
+  def move(circle: Vector[Int]): Vector[Int] = {
 //    println(System.currentTimeMillis())
-    val current = circle.head
+    val current = circle(0)
     val take1 = circle(1)
     val take2 = circle(2)
     val take3 = circle(3)
 
-//    println(System.currentTimeMillis())
+    println(System.currentTimeMillis())
     val tail = circle.drop(4)
 
 //    println(System.currentTimeMillis())
@@ -24,7 +24,6 @@ object _23 {
 //      tailWithIndex.maxBy(_._1) }
 
 //    println(System.currentTimeMillis())
-    // problem starts here
 //    val destinationIndex = tailWithIndex
 //      .filter(_._1 < current).maxByOption(_._1).getOrElse(tuple)
 //      ._2 + 1
@@ -33,34 +32,54 @@ object _23 {
 
     val avoid = Set(current,take1,take2,take3)
     def findNext (candidate:Int):Int={
-      println(candidate)
+//      println(candidate)
       if (avoid contains candidate)
         findNext(if (candidate == 1) 9 else candidate - 1)
       else candidate
     }
 
+//    println(System.currentTimeMillis())
     val destinationIndex = tail.indexOf(findNext(current)) + 1
 
-    // until here
+    // The problem has moved to here
 
-//    println(System.currentTimeMillis())
-    val result = tail.to(LazyList).take(destinationIndex).appended(take1).appended(take2).appended(take3)
-      .concat(tail.drop(destinationIndex)).appended(current).to(List)
+    println(System.currentTimeMillis())
+
+    val a = tail.take(destinationIndex)
+    val b = tail.drop(destinationIndex)
+    val result=  Vector.concat(
+      a,
+      Seq(take1),
+      Seq(take2),
+      Seq(take3),
+      b,
+      Seq(current))
+
+//    val result=List.concat(
+//      tail.to(LazyList).take(destinationIndex),
+//      List(take1),
+//      List(take2),
+//      List(take3),
+//      tail.drop(destinationIndex),
+//      List(current))
+
+//    val result = tail.to(LazyList).take(destinationIndex).appended(take1).appended(take2).appended(take3)
+//      .concat(tail.drop(destinationIndex)).appended(current).to(List)
 
 //    println(System.currentTimeMillis())
 //    System.exit(-1)
     result
   }
 
-  def moveRec(circle: List[Int], movesLeft: Int): List[Int] =
+  def moveRec(circle: Vector[Int], movesLeft: Int): Vector[Int] =
     if (movesLeft == 0) circle
     else {
-      /*if (movesLeft % 1000 == 0) */println(s"$movesLeft moves left")
+      if (movesLeft % 1000 == 0) println(s"$movesLeft moves left @${System.currentTimeMillis()}")
       moveRec(move(circle), movesLeft - 1)
   }
 
   def _1(input: String): String = {
-    val circle = input.toList.map(_.toString.toInt)
+    val circle = input.toVector.map(_.toString.toInt)
     val endCircle = moveRec(circle, 100)
 
     val index = endCircle.indexOf(1)
@@ -68,7 +87,7 @@ object _23 {
   }
 
   def _2(input: String): Long = {
-    val circle = input.toList.map(_.toString.toInt).concat(10 to 1000000)
+    val circle = input.toVector.map(_.toString.toInt).concat(10 to 1000000)
     val endCircle = moveRec(circle, 10000000)
 
     val index = endCircle.indexOf('1')
