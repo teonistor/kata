43b258a8aed8b9d75cfcd3878a8aeed13fc32562
Teo G Nistor <teo.g.nistor@gmail.com>
2021-01-27 22:34:42 +0000

    AdventOfCode: Day 25

--- src/main/scala/io/github/teonistor/adventofcode/_25.scala
+++ src/main/scala/io/github/teonistor/adventofcode/_25.scala
@@ -4,56 +4,53 @@ import scala.collection.mutable
 
 object _25 {
 
-  // Not sure if this is adding much benefit to this problem though
-  private val memo = mutable.LinkedHashMap.empty[(Int,Int,Int), Int]
-
-  private def transform(seed: Int, subject: Int, loops: Int, cap: Int): Int =
-      if (loops == 0)
-        seed
-      else{
-        if (loops % 100 == 0) println(s"Loop count $loops")
-        transform(transform(seed, subject, cap), subject, loops - 1, cap)}
+  private def seek(seed: Int, subject: Int, cap: Int, loopCount: Int, key1:Int,key2:Int,result1:Option[Int], result2:Option[Int]): (Int,Int) =
+    if (result1.isDefined&&result2.isDefined)
+      (result1.get,result2.get)
+    else{
+      val possibleResult1 = result1.orElse(if (seed==key1)Option(loopCount) else Option.empty)
+      val possibleResult2 = result2.orElse(if (seed==key2)Option(loopCount) else Option.empty)
+      seek(transform(seed, subject, cap), subject, cap, loopCount+1, key1, key2, possibleResult1 ,possibleResult2)
+     }
 
   private def transform(seed: Int, subject: Int, cap: Int) =
-    if (memo contains (seed, subject,  cap))
-      memo((seed, subject,  cap))
-else
+
     {val v=(seed.toLong * subject.toLong) % cap.toLong
-  memo.put((seed, subject,  cap), math.toIntExact(v))
       math.toIntExact(v)}
 
   def _1(input: String): Int = {
 
-//    LazyList.iterate(1)(transform(_,7,20201227))
-    val keys = input.split('\n').map(_.toInt).zipWithIndex.toMap
+    val keys = input.split('\n').map(_.toInt)
 
-    val loops = LazyList.iterate(0)(_ + 1)
-      .map(loops => keys.get(transform(1, 7, loops, 20201227)).map((_, loops)))
-      .filter(_.isDefined)
-      .map(_.get)
-      .take(2)
-      .toMap
+    val (result1, result2) = seek(1, 7,20201227, 0, keys(0), keys(1), Option.empty, Option.empty)
 
-    println(s"Loops: $loops")
+    val encryptionKey = Set(
+        (keys(0), result2),
+        (keys(1),result1)    )
+      .map(keyAndLoopCount => LazyList.iterate(1)(transform(_, keyAndLoopCount._1, 20201227)).drop(keyAndLoopCount._2).head)
+//
+//
+//
+//    val loops = LazyList.iterate(0)(_ + 1)
+//      .map(loops => keys.get(transform(1, 7, loops, 20201227)).map((_, loops)))
+//      .filter(_.isDefined)
+//      .map(_.get)
+//      .take(2)
+//      .toMap
+//
+//    println(s"Loops: $loops")
 
 //    LazyList.iterate(0)(_ + 1).filter(l => transform(keys.head._1, 7, loops((keys.head._2 + 1) % 2) + l, 20201227)== 14897079)
 //      .take(1)
 //      .foreach(println)
 
-    val encryptionKey = keys
-      .map(seedAndI => transform(1, seedAndI._1, loops((seedAndI._2 + 1) % 2), 20201227)).toSet
+//    val encryptionKey = keys
+//      .map(seedAndI => transform(1, seedAndI._1, loops((seedAndI._2 + 1) % 2), 20201227)).toSet
 
     if (encryptionKey.size != 1)
       throw new RuntimeException(s"Postcondition failure: Both devices should arrive at the same key but ${encryptionKey.size} found!")
 
-//    println(s"All memo: ${memo.to(LazyList).sortBy(_._1).to(List)}")
-//    println(s"All memo: $memo")
-    memo.zipWithIndex.foreach(u => println(s"${u._2}: ${u._1._1._1} -> ${u._1._2}"))
     encryptionKey.head
-  }
-
-  def _2(input: String): Int = {
-    0
   }
 }
 
---
+++ src/test/scala/io/github/teonistor/adventofcode/_25Test.scala
@@ -0,0 +1,9 @@
+package io.github.teonistor.adventofcode
+
+class _25Test extends AdventOfCodeTestBase {
+  testAndRun(25, 1, _25._1,
+    "5764801\n17807724",
+    14897079,
+    "3469259\n13170438"
+  )
+}
