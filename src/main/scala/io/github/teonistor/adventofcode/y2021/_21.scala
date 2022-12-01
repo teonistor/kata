e14e5987dc95c9d369b7f3ba590fad2bd62330b6
Teodor G Nistor <teodor@nistor.uk>
2022-12-01 23:22:48 +0000

    Day 21 part 2

--- src/main/scala/io/github/teonistor/adventofcode/y2021/_21.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2021/_21.scala
@@ -40,8 +40,44 @@ object _21 {
 
   }
 
+  // This takes about 100 seconds and can be improved
   def _2(input: String): Long = {
-    println("Unimplemented")
-    0
+    val poss = input.split("\n").map {
+      case startPosReader(player, position) => (player.toInt, position.toInt)
+    } .sorted
+      .map(_._2)
+//      .to(Vector)
+
+    val rolls = (1 to 3).flatMap(a =>
+      (1 to 3).flatMap(b =>
+        (1 to 3).map(c =>
+          a + b + c)))
+      .groupMapReduce(identity)(_ => 1)(_+_)
+      .to(LazyList)
+
+    def stuff(nextPlayer: Int,
+              nextPlayerPosition: Int,
+              nextPlayerScore: Int,
+              otherPlayerPosition: Int,
+              otherPlayerScore: Int,
+              universes: Int): Map[Int,Long] =
+      if (otherPlayerScore >= 21)
+        Map(((nextPlayer + 1) % 2, universes))
+      else {
+        rolls.flatMap(rollAndUniverses => {
+          val newPos = (nextPlayerPosition + rollAndUniverses._1 - 1) % 10 + 1
+          val newScore = nextPlayerScore + newPos
+          stuff((nextPlayer + 1) % 2, otherPlayerPosition, otherPlayerScore, newPos, newScore, rollAndUniverses._2)
+            .view
+            .mapValues(_ * universes)
+        }).groupMapReduce(_._1)(_._2)(_+_)
+      }
+
+    stuff(0, poss(0), 0, poss(1), 0, 1)
+      .values
+      .max
+  }
+
+  def main(arg: Array[String]): Unit = {
   }
 }
--- src/test/scala/io/github/teonistor/adventofcode/y2021/_21Test.scala
+++ src/test/scala/io/github/teonistor/adventofcode/y2021/_21Test.scala
@@ -6,7 +6,7 @@ class _21Test extends AdventOfCodeTestBase {
   testAndRun(21, _21._1, _21._2,
     "Player 1 starting position: 4\nPlayer 2 starting position: 8",
     739785L,
-    0L,
+    444356092776315L,
     "Player 1 starting position: 4\nPlayer 2 starting position: 6"
   )
 }
