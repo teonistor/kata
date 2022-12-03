56a0cc609cea99f7fb4482f2e4e1ef6893df41a0
Teodor G Nistor <teodor@nistor.uk>
2022-12-03 20:10:59 +0000

    Day 3: refactor

--- src/main/scala/io/github/teonistor/adventofcode/y2022/_03.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2022/_03.scala
@@ -1,24 +1,31 @@
 package io.github.teonistor.adventofcode.y2022
 
 import io.github.teonistor.adventofcode.AdventOfCodeSolution
+import io.github.teonistor.func.Utils.HasLet
 
 object _03 extends AdventOfCodeSolution[Int](3) {
+  private type PartFunc = Seq[String] => Iterable[Iterable[String]]
 
-  def _1(input: String): Int = input
-    .split("\n").to(LazyList)
-    .map(sack => sack.splitAt(sack.length / 2))
-    .map(compartments => compartments._1.toSet & compartments._2.toSet)
-    .map(_.head)
-    .map(c => if (c >= 'a') c - 'a' + 1 else c - 'A' + 27)
-    .sum
+  def _1(input: String): Int =
+    solve(input, part1SpecialLogic)
 
-  def _2(input: String): Int = input
+  def _2(input: String): Int =
+    solve(input, part2SpecialLogic)
+
+  private def solve(input: String, partSpecialLogic: PartFunc): Int = input
     .split("\n").to(LazyList)
-    .grouped(3)
-    .map(_
-      .map(_.toSet)
+    .let(partSpecialLogic)
+    .map(_.map(_.toSet)
       .reduce(_ & _)
       .head)
     .map(c => if (c >= 'a') c - 'a' + 1 else c - 'A' + 27)
     .sum
+
+  private def part1SpecialLogic(sacks: Seq[String]) = sacks
+    .map(sack => sack.splitAt(sack.length / 2))
+    .map(compartments => List(compartments._1, compartments._2))
+
+  private def part2SpecialLogic(sacks: Seq[String]) = sacks
+    .grouped(3)
+    .to(LazyList)
 }
