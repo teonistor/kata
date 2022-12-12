39d12260488dd5a22e4c8c47e5114a07a709cf6c
Teodor G Nistor <teodor@nistor.uk>
2022-12-12 22:57:45 +0000

    Day 12: refactor

--- src/main/scala/io/github/teonistor/adventofcode/y2022/_12.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2022/_12.scala
@@ -7,53 +7,51 @@ import scala.collection.immutable.Queue
 
 object _12 extends AdventOfCodeSolution[Int] {
 
-  def _1(input: String): Int = {
-    val chart = input.split("\n")
-//    val start = findLetter(chart, 'S')
-    val end = findLetter(chart, 'E')
+  def _1(input: String): Int =
+    solve0(input, 'S')
 
-    navigate(ij => chart.lift(ij._1).flatMap(_.lift(ij._2)), 'S', Map(end -> 0), Queue(end))
-  }
+  def _2(input: String): Int =
+    solve0(input, 'a')
 
-  def _2(input: String): Int = {
-    val chart = input.split("\n")
-    val end = findLetter(chart, 'E')
+  private def solve0(input: String, target: Char) =
+    solve1(input.split("\n"), target)
 
-    navigate(ij => chart.lift(ij._1).flatMap(_.lift(ij._2)), 'a', Map(end -> 0), Queue(end))
-  }
-
-  private def findLetter(chart: Array[String], letter: Char) =
-    chart.indices.flatMap(i =>
-      chart(i).indices.filter(j =>
-        chart(i)(j) == letter)
+  private def solve1(chart: Array[String], target: Char) =
+    solve2(chart, target,
+      chart.indices.flatMap(i =>
+        chart(i).indices.filter(j =>
+          chart(i)(j) == 'E')
         .map((i, _)))
-      .head
+      .head)
+
+  private def solve2(chart: Array[String], target: Char, startFrom: (Int, Int)) =
+    navigate(ij => chart.lift(ij._1).flatMap(_.lift(ij._2)), target, Map(startFrom -> 0), Queue(startFrom))
 
   @tailrec
   private def navigate(chart: ((Int, Int)) => Option[Char],
                        target: Char,
                        steps: Map[(Int, Int), Int],
                        queue: Queue[(Int, Int)]): Int = {
-    val current = queue.head
-    if (chart(current).get == target)
-      steps(current)
-    else {
-      val next = (udlr _ tupled current)
-        .filterNot(steps.contains)
-        .filter(ij => {
-          val c = chart(ij)
-          c.exists(canNavigate(chart(current).get, _))
-        })
+    val currentIJ = queue.head
+    val currentChar = chart(currentIJ).get
 
-      navigate(chart, target, steps concat next.map((_, steps(current) + 1)), queue.tail concat next)
+    if (currentChar == target)
+      steps(currentIJ)
+
+    else {
+      val next = (udlr _ tupled currentIJ)
+        .filterNot(steps.contains)
+        .filter(chart(_).exists(canNavigate(currentChar, _)))
+
+      navigate(chart, target, steps concat next.map((_, steps(currentIJ) + 1)), queue.tail concat next)
     }
   }
 
   private def udlr(i: Int, j: Int) = LazyList(
-    (i + 1, j),
     (i - 1, j),
-    (i, j + 1),
-    (i, j - 1))
+    (i + 1, j),
+    (i, j - 1),
+    (i, j + 1))
 
   private def canNavigate(from:Char, to:Char) =
     (if (from == 'E') 'z' else from) -
