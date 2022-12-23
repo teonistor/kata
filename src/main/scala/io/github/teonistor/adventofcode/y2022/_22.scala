3eac72065c5451fb449f1dbf867a958eb3a5944c
Teodor G Nistor <teodor@nistor.uk>
2022-12-23 22:12:57 +0200

    Day 22 part 2

--- src/main/scala/io/github/teonistor/adventofcode/y2022/_22.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2022/_22.scala
@@ -72,13 +72,41 @@ object _22 extends AdventOfCodeSolution[Int] {
   }
 
   private def lookupWarpSmall(i: Int, j: Int, rot: Int) =
-    (rot, i/4, j/4) match {
+    (rot, i / 4, j / 4) match {
       case (0, 0, _) => (11 - i, 15, 2)
-      case (0, 1, _) => ()
+      case (0, 1, _) => (8, 19 - i, 1)
+      case (0, 2, _) => (11 - i, 11, 2)
+      case (1, _, 0) => (11, 11 - j, 3)
+      case (1, _, 1) => (15 - j, 8, 0)
+      case (1, _, 2) => (7, 11 - j, 3)
+      case (1, _, 3) => (19 - j, 0, 0)
+      case (2, 0, _) => (4, 4 + i, 1)
+      case (2, 1, _) => (11, 19 - i, 3)
+      case (2, 2, _) => (7, 15 - i, 3)
+      case (3, _, 0) => (0, 11 - j, 1)
+      case (3, _, 1) => (j - 4, 8, 0)
+      case (3, _, 2) => (4, 11 - j, 1)
+      case (3, _, 3) => (19 - j, 11, 2)
     }
 
   // This is specific to my input. Good enough for the demo :P
-//  def lookupWarpLarge
+  def lookupWarpLarge(i: Int, j: Int, rot: Int) =
+    (rot, i / 50, j / 50) match {
+      case (0, 0, _) => (149 - i, 99, 2)
+      case (0, 1, _) => (49, i + 50, 3)
+      case (0, 2, _) => (149 - i, 149, 2)
+      case (0, 3, _) => (149, i - 100, 3)
+      case (1, _, 0) => (0, 100 + j, 1)
+      case (1, _, 1) => (100 + j, 49, 2)
+      case (1, _, 2) => (j - 50, 99, 2)
+      case (2, 0, _) => (149 - i, 0, 0)
+      case (2, 1, _) => (100, i - 50, 1)
+      case (2, 2, _) => (149 - i, 50, 0)
+      case (2, 3, _) => (0, i - 100, 1)
+      case (3, _, 0) => (j + 50, 50, 0)
+      case (3, _, 1) => (j + 100, 0, 0)
+      case (3, _, 2) => (199, j - 100, 3)
+    }
 
   def _2(input: String): Int = {
 
@@ -96,7 +124,7 @@ object _22 extends AdventOfCodeSolution[Int] {
         case (null, "R") => Right(_.right())
       }
 
-//    def lookupWarp
+    val lookupWarp = if (input.length < 250) lookupWarpSmall _ else lookupWarpLarge _
 
     @tailrec
     def thing(instructions: Seq[Either[Int, Composure => Composure]], c: Composure): Composure = {
@@ -107,12 +135,18 @@ object _22 extends AdventOfCodeSolution[Int] {
       else instructions.head match {
         case Left(0) => thing(instructions.tail, c)
         case Left(n) =>
-          val oneStep = LazyList.iterate(c.safeMove())(_.safeMove())
-            .find(oneStep => map(oneStep.i)(oneStep.j) != ' ').get
-          map(oneStep.i)(oneStep.j) match {
-            case ' ' => thing(instructions, oneStep)
-            case '.' => thing(Left(n - 1) +: instructions.tail, oneStep)
-            case '#' => thing(instructions.tail, c)
+          val oneStep = c.move()
+          map.lift(oneStep.i)
+            .flatMap(_.lift(oneStep.j)) match {
+              case Some('.') => thing(Left(n - 1) +: instructions.tail, oneStep)
+              case Some('#') => thing(instructions.tail, c)
+              case _ =>
+                val (warpedI, warpedJ, warpedRot) = lookupWarp(oneStep.i, oneStep.j, oneStep.rot)
+                println(s"i=$warpedI, j=$warpedJ, rot=$warpedRot")
+                map(warpedI)(warpedJ) match {
+                  case '.' => thing(Left(n - 1) +: instructions.tail, c.copy(i = warpedI, j = warpedJ, rot = warpedRot))
+                  case '#' => thing(instructions.tail, c)
+                }
           }
         case Right(op) => thing(instructions.tail, op(c))
 
