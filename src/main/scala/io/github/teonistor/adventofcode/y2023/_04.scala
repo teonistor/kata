e1bbcfc7d24becb07ee4e9b980655f21df6c349a
Teodor G Nistor <teodor@nistor.uk>
2023-12-04 18:46:22 +0000

    AoC2023: Refactor day 4

--- src/main/scala/io/github/teonistor/adventofcode/y2023/_04.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_04.scala
@@ -2,50 +2,41 @@ package io.github.teonistor.adventofcode.y2023
 
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
-import scala.collection.mutable
+import scala.annotation.tailrec
 
 object _04 extends StandardAdventOfCodeSolution[Long]{
-  private val splitter = "Card +(\\d+):(.+)\\|(.+)".r
 
   override def _1(input: String): Long =
-    input.split("\n").iterator
-      .map(_.replaceAll("Card +\\d+:", "")
-      .split('|').iterator
-      .map(_.strip()
-      .split(" +").iterator
-      .map(_.toInt)
-      .toSet)
-      .reduce(_&_))
-      .map(s => score(s.size))
+    parseInput(input).iterator
+      .map(directScore(_))
       .sum
 
   override def _2(input: String): Long = {
-    val winningness = input.split("\n").map {
-      case splitter(card, winningNumbers, numbersHad) => (card.toInt, Iterator(winningNumbers, numbersHad)
-      .map(_.strip()
-        .split(" +").iterator
-        .map(_.toInt)
-        .toSet)
+    val ticketsGiven = parseInput(input)
+    val ticketsHad = Array.fill(ticketsGiven.length)(1)
+
+    // FIXME Mutable
+    ticketsHad.indices
+      .foreach(i => (i + 1 to i + ticketsGiven(i))
+        .foreach(j => ticketsHad(j) += ticketsHad(i)))
+
+    ticketsHad.sum
+  }
+
+  private def parseInput(input: String) =
+    input.split("\n")
+      .map(_.replaceAll("Card +\\d+:", "")
+        .split('|').iterator
+        .map(_.strip()
+          .split(" +").iterator
+          .map(_.toInt)
+          .toSet)
         .reduce(_&_)
         .size)
-    }
 
-    // TODO Mutable
-    val howMany = winningness.map {
-      case (k,_) => (k,1)
-    }.to(mutable.Map)
-
-    winningness.foreach {
-      case (k,v) => (1 to v).map(_ + k).foreach(x => howMany(x) += howMany(k))
-    }
-
-    howMany.values.sum
-    // 7411 too low
-  }
-
-  private def score(i:Int):Long = {
+  @tailrec
+  private def directScore(i: Int, base: Long = 1): Long =
     if (i < 1) 0
-    else if (i == 1) 1
-    else 2 * score(i-1)
-  }
+    else if (i == 1) base
+    else directScore(i-1, base*2)
 }
