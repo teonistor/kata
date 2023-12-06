59fb7f21a0b1d443517925b6fd6f99aba00b55fc
Teodor G Nistor <teodor@nistor.uk>
2023-12-06 22:51:06 +0000

    AoC2023: Refactor day 6

--- src/main/scala/io/github/teonistor/adventofcode/y2023/_06.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_06.scala
@@ -3,16 +3,22 @@ package io.github.teonistor.adventofcode.y2023
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
 import java.lang.Math.{ceil, floor, sqrt}
-import java.math.MathContext
 
 object _06 extends StandardAdventOfCodeSolution[Long]{
 
+  override def _1(input: String): Long =
+    solve(input, _.replaceAll("(Time|Distance):", "").strip().split("\\s+"))
 
-  override def _1(input: String): Long = {
-    val timeRow :: distanceRow :: Nil = input.split("\n").toList
-    val times = timeRow.replace("Time:", "").strip().split("\\s+").map(_.toLong)
-    val distances = distanceRow.replace("Distance:", "").strip().split("\\s+").map(_.toLong)
-    (times.iterator zip distances.iterator).map { case (distance, time) =>
+  override def _2(input: String): Long =
+    solve(input, s => Some(s.replaceAll("[^0-9]", "")))
+
+  private def solve(input:String, parseMode:String=>Iterable[String]) = {
+    val times :: distances :: Nil = input.split("\n").iterator
+      .map(parseMode.andThen(_.iterator
+        .map(_.toLong)))
+      .toList
+
+    (times zip distances).map { case (distance, time) =>
       val delta = distance * distance - 4 * time
       if (delta < 0)
         throw new IllegalStateException(s"No real roots for $distance, $time")
@@ -20,47 +26,8 @@ object _06 extends StandardAdventOfCodeSolution[Long]{
       val sqrtdd = sqrt(delta) / 2
       val nd = distance.toDouble / 2
 
-      val min = floor(nd - sqrtdd)
-      val max = ceil(nd + sqrtdd)
-      max.toLong - min.toLong - 1
+      ceil(nd + sqrtdd).toLong - floor(nd - sqrtdd).toLong - 1
     }
       .product
   }
-
-  override def _2(input: String): Long = {
-
-    val timeRow :: distanceRow :: Nil = input.split("\n").toList
-    val times = timeRow.replace("Time:", "").replaceAll("\\s+", "").strip().split("\\s+").map(_.toLong)
-    val distances = distanceRow.replace("Distance:", "").strip().split("\\s+").map(_.toLong)
-    (times.iterator zip distances.iterator).map { case (distance, time) =>
-        val delta = distance * distance - 4 * time
-        if (delta < 0)
-          throw new IllegalStateException(s"No real roots for $distance, $time")
-
-        val sqrtdd = BigDecimal(BigDecimal(delta).bigDecimal.sqrt(MathContext.DECIMAL128)) / 2
-        val nd = BigDecimal(distance) / 2
-
-        val min = /*floor*/(nd - sqrtdd).bigDecimal.longValue()
-        val max = /*ceil*/(nd + sqrtdd).bigDecimal.longValue()
-        max-min
-      }
-      .product
-  }
-
-//  val time = new math.BigDecimal("44806572")
-//  val distance = new math.BigDecimal("208158110501102")
-//
-//  // -x^2 + time * x - distance > 0
-//
-//  val d2 = time.multiply(new math.BigDecimal("0.5"))
-//  val dsqd = time.multiply(time).divide(new math.BigDecimal(4)).subtract(distance).sqrt(MathContext.DECIMAL128)
-//
-//  val min = d2.subtract(dsqd)
-//  val max = d2.add(dsqd)
-//
-//  println(min)
-//  println(max)
-//  println(39542396 - 5264175)
-
-  // 34278221
 }
