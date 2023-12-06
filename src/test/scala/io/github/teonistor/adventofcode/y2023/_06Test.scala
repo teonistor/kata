a316c28d7d2025bc883de55cf7dbde1607b9de64
Teodor G Nistor <teodor@nistor.uk>
2023-12-06 22:36:23 +0000

    AoC2023: Day 6, broken (but commented code had worked elsewhere)

---
+++ src/test/scala/io/github/teonistor/adventofcode/y2023/_06Test.scala
@@ -0,0 +1,7 @@
+package io.github.teonistor.adventofcode.y2023
+
+import io.github.teonistor.adventofcode.AdventOfCodeTestBase
+
+class _06Test extends AdventOfCodeTestBase {
+  autorun(_06, 288L, 71503L,"Time:      7  15   30\nDistance:  9  40  200")
+}
