e2f8a798e8a5fc35be738af16720a752f3af4e20
Teodor G Nistor <teodor@nistor.uk>
2023-12-09 22:36:20 +0000

    Util: Prime decomposition helper

--- src/main/scala/io/github/teonistor/func/Utils.scala
+++ src/main/scala/io/github/teonistor/func/Utils.scala
@@ -1,5 +1,7 @@
 package io.github.teonistor.func
 
+import scala.annotation.tailrec
+
 // This should make it into our library of utils
 object Utils {
 
@@ -7,4 +9,17 @@ object Utils {
 
     def let[U <: Any](func: T => U): U = func(obj)
   }
+
+  @tailrec
+  def primes(input: Long, candidate: Long = 2, output: Map[Long, Int] = Map.empty): Map[Long, Int] =
+    if (input == 0)
+      Map(0L -> 1)  // Esoteric :P
+    else if (input < 0)
+      primes(-input, candidate, output.updated(-1L, 1))  // Esoteric :P
+    else if (input < 2)
+      output
+    else if (input % candidate != 0)
+      primes(input, candidate + 1, output)
+    else
+      primes(input / candidate, candidate, output.updatedWith(candidate)(_.map(_ + 1).orElse(Some(1))))
 }
