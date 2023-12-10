9e323911a3b90e3b7496a1a5b33db60a9229fcc0
Teodor G Nistor <teodor@nistor.uk>
2023-12-10 06:38:31 +0000

    AoC2023: Day 10 part 2

--- src/main/scala/io/github/teonistor/adventofcode/y2023/_10.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_10.scala
@@ -2,6 +2,8 @@ package io.github.teonistor.adventofcode.y2023
 
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
+import scala.collection.immutable.Queue
+
 object _10 extends StandardAdventOfCodeSolution[Int] {
 
   private val pipes = Map(
@@ -14,22 +16,44 @@ object _10 extends StandardAdventOfCodeSolution[Int] {
     .view.mapValues { case (a,b) => Map(a -> b, b -> a)}
     .toMap
 
-  override def _1(input: String): Int = {
+  private val pipeExpansion = Map(
+    'S' -> """.#.
+             |###
+             |.#.""".stripMargin.split("\n"),
+    '|' -> """.#.
+             |.#.
+             |.#.""".stripMargin.split("\n"),
+    '-' -> """...
+             |###
+             |...""".stripMargin.split("\n"),
+    'L' -> """.#.
+             |.##
+             |...""".stripMargin.split("\n"),
+    'J' -> """.#.
+             |##.
+             |...""".stripMargin.split("\n"),
+    '7' -> """...
+             |##.
+             |.#.""".stripMargin.split("\n"),
+    'F' -> """...
+             |.##
+             |.#.""".stripMargin.split("\n"))
+
+  private def findPipe(input: String) = {
     val map = input.split("\n")
     val startingRow = map.indexWhere(_.contains('S'))
     val startingCol = map(startingRow).indexOf('S')
 
     val mm = ((row: Int, col: Int) => map.lift(row).flatMap(_.lift(col))).tupled
 
-    def navigate(intent: Set[((Int, Int), (Int, Int))], stepsCount: Int): Int =
-      if (intent.map(_._2).size < 2)
-        stepsCount
+    def navigate(movements: Set[List[(Int, Int)]]): Set[(Int, Int)] =
+      if (movements.map(_.head).size < 2)
+        movements.flatten
       else
         navigate(
-          intent.flatMap {
-            case (previous, current) => navigate1(previous, current).map((current, _))
-          }, stepsCount + 1
-        )
+          movements.flatMap {
+            case current :: previous :: rest => navigate1(previous, current).map(_ :: current :: previous :: rest)
+          })
 
     def navigate1(previous: (Int, Int), current: (Int, Int)): Option[(Int, Int)] =
       mm(current).flatMap(pipes.get)
@@ -37,12 +61,48 @@ object _10 extends StandardAdventOfCodeSolution[Int] {
         .map(d => (d._1+current._1, d._2+current._2))
 
     navigate(Set(
-      ((startingRow, startingCol), (startingRow - 1, startingCol)),
-      ((startingRow, startingCol), (startingRow + 1, startingCol)),
-      ((startingRow, startingCol), (startingRow, startingCol - 1)),
-      ((startingRow, startingCol), (startingRow, startingCol + 1))), 1)
-
+      List((startingRow - 1, startingCol), (startingRow, startingCol)),
+      List((startingRow + 1, startingCol), (startingRow, startingCol)),
+      List((startingRow, startingCol - 1), (startingRow, startingCol)),
+      List((startingRow, startingCol + 1), (startingRow, startingCol))))
+     .incl((startingRow, startingCol))
   }
 
-  override def _2(input: String): Int = ???
+  override def _1(input: String): Int =
+    findPipe(input).size / 2
+
+  override def _2(input: String): Int = {
+    val map = input.split("\n")
+    val pipe = findPipe(input)
+
+    val expandedMap = Array.fill(map.length*3, map(0).length*3)('.')
+    pipe.foreach { case (row,col) =>
+      (0 to 2).foreach(dr =>
+        (0 to 2).foreach(dc =>
+      expandedMap(row*3+dr)(col*3+dc) = pipeExpansion(map(row)(col))(dr)(dc)))
+    }
+
+    def markReach(where:Queue[(Int,Int)]): Unit = {
+      if (where.nonEmpty) {
+        val (row, col) = where.head
+        if (expandedMap.lift(row).flatMap(_.lift(col)).contains('.')) {
+          expandedMap(row)(col) = '#'
+          markReach(where.tail.appendedAll(Iterator((row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col))))
+
+        } else
+          markReach(where.tail)
+      }
+    }
+
+//    expandedMap.foreach(u => println(u.mkString))
+    markReach(Queue((0,0), (0,expandedMap(0).length-1), (expandedMap.length-1,0), (expandedMap.length-1,expandedMap(0).length-1)))
+//    expandedMap.foreach(u => println(u.mkString))
+
+    expandedMap.indices.filter(_%3 == 1)
+      .map(row => expandedMap(row).indices.filter(_%3 == 1)
+        .count(col => expandedMap(row)(col) == '.'))
+      .sum
+  }
 }
+
+// 7005
--- src/test/scala/io/github/teonistor/adventofcode/y2023/_10Test.scala
+++ src/test/scala/io/github/teonistor/adventofcode/y2023/_10Test.scala
@@ -5,4 +5,15 @@ import io.github.teonistor.adventofcode.AdventOfCodeTestBase
 class _10Test extends AdventOfCodeTestBase {
   autorun(_10, 8, 0, "..F7.\n.FJ|.\nSJ.L7\n|F--J\nLJ...")
 
+  test("part 2 Small example") {
+    assert(_10._2("...........\n.S-------7.\n.|F-----7|.\n.||.....||.\n.||.....||.\n.|L-7.F-J|.\n.|..|.|..|.\n.L--J.L--J.\n...........") == 4)
+  }
+
+  test("part 2 Medium example") {
+    assert(_10._2(".F----7F7F7F7F-7....\n.|F--7||||||||FJ....\n.||.FJ||||||||L7....\nFJL7L7LJLJ||LJ.L-7..\nL--J.L7...LJS7F-7L7.\n....F-J..F7FJ|L7L7L7\n....L7.F7||L7|.L7L7|\n.....|FJLJ|FJ|F7|.LJ\n....FJL-7.||.||||...\n....L---J.LJ.LJLJ...") == 8)
+  }
+
+  test("part 2 Large example") {
+    assert(_10._2("FF7FSF7F7F7F7F7F---7\nL|LJ||||||||||||F--J\nFL-7LJLJ||||||LJL-77\nF--JF--7||LJLJ7F7FJ-\nL---JF-JLJ.||-FJLJJ7\n|F|F-JF---7F7-L7L|7|\n|FFJF7L7F-JF7|JL---7\n7-L-JL7||F7|L7F-7F7|\nL.L7LFJ|||||FJL7||LJ\nL7JLJL-JLJLJL--JLJ.L") == 10)
+  }
 }
