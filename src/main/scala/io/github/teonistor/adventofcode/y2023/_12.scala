b471f218386a173e73c474651e1499ea6c9d8aba
Teodor G Nistor <teodor@nistor.uk>
2023-12-12 20:58:17 +0000

    AoC2023: Day 12 not working, but passing example and all tests I can come up with which is very annoying

---
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_12.scala
@@ -0,0 +1,44 @@
+package io.github.teonistor.adventofcode.y2023
+
+import com.google.common.annotations.VisibleForTesting
+import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
+
+object _12 extends StandardAdventOfCodeSolution[Long] {
+
+  override def _1(input: String): Long = {
+    input.split('\n').iterator
+      .map(solveOne)
+      .sum
+  }
+
+  override def _2(input: String): Long = ???
+
+
+  @VisibleForTesting
+  def solveOne(row: String): Long = {
+    val report :: summaryStr :: Nil = row.split(' ').toList
+    solveRecu(report, 0, summaryStr.split(',').toList.map(_.toInt))
+  }
+
+  private def solveRecu(report: String, pos: Int, remaining: List[Int]): Long = {
+    if (remaining.isEmpty)
+      if (pos < report.length && report.substring(pos).contains('#'))
+        0
+      else
+        1
+    else if (pos >= report.length || report.substring(pos).forall(_ == '.'))
+      0
+
+    else {
+      val endPos = pos + remaining.head
+      if (endPos > report.length || report.substring(pos, endPos).contains('.') || endPos < report.length && report(endPos) == '#')
+        solveRecu(report, pos + 1, remaining)
+      else if (report(pos) == '#')
+        solveRecu(report, endPos + 1, remaining.tail)
+      else
+        solveRecu(report, pos + 1, remaining) + solveRecu(report, endPos + 1, remaining.tail)
+    }
+  }
+}
+
+// between 3789 and 7505
