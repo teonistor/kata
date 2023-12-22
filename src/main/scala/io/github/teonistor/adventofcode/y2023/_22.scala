d39061281105c8380d44b8451bc426cdd2dd5570
Teodor G Nistor <teodor@nistor.uk>
2023-12-22 16:23:50 +0000

    AoC2023: Day 22 part 1 - better

--- src/main/scala/io/github/teonistor/adventofcode/y2023/_22.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_22.scala
@@ -2,14 +2,28 @@ package io.github.teonistor.adventofcode.y2023
 
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
+import scala.collection.mutable
+
 object _22 extends StandardAdventOfCodeSolution[Int] {
 
   private val splitter = "(\\d+),(\\d+),(\\d+)~(\\d+),(\\d+),(\\d+)".r
 
   private case class Brick(points: Set[(Int,Int,Int)]) {
-    lazy val lowestHeight = points.map(_._3).min
+    val lowestHeight = points.map(_._3).min
+    lazy val planarProjection = points.map { case (x,y,_) => (x,y) }
     lazy val lowestProjection = points.map { case (x,y,_) => (x,y,lowestHeight)}
     lazy val floorProjection = lowestProjection.map { case (x,y,z) => (x,y,z-1)}
+
+    def fall(howMuch: Int) = {
+      if (howMuch < 0)
+        println("Busted!")
+      if (howMuch > 0)
+        Brick(points.map {
+          case (x, y, z) => (x, y, z - howMuch)
+        })
+      else
+        this
+    }
   }
 
   override def _1(input: String): Int = {
@@ -21,33 +35,53 @@ object _22 extends StandardAdventOfCodeSolution[Int] {
            (z1.toInt to z2.toInt).map((x, y, _)))).toSet)
    }.toSet
 
-    val settledBricks = fall(bricks)
+    val que = mutable.PriorityQueue.newBuilder(Ordering.by(-(_:Brick).lowestHeight))
+      .addAll(bricks)
+      .result()
+
+
+    val settledBricks = fall(que)
     val leanOn = settledBricks.map(brick =>(brick, settledBricks.filter(_.points.exists(it => brick.floorProjection contains it))))
       .toMap
 
     val aye = leanOn.values.toSet
       .filter(_.size == 1)
 
-    println(s"Bricks size ${bricks.size}")
-    println(s"Settled bricks size ${settledBricks.size}")
-    println(s"LeanOn size ${leanOn.size}")
-
-    println(leanOn)
-    println(aye)
+//    println(s"Bricks size ${bricks.size}")
+//    println(s"Settled bricks size ${settledBricks.size}")
+//    println(s"LeanOn size ${leanOn.size}")
+//
+//    println(leanOn)
+//    println(aye)
 
     bricks.size - aye.size
   }
 
   override def _2(input: String): Int = ???
 
-  private def fall(bricks: Set[Brick]): Set[Brick]={
-    bricks.find(brick => brick.lowestHeight > 1 && !bricks.exists(_.points overlaps brick.floorProjection)) match {
-      case None => bricks
-      case Some(brick) => fall(bricks - brick + Brick(brick.points.map {
-        case (x, y, z) => (x, y, z - 1)
-      }))
-    }
+  private def fall(floating: mutable.PriorityQueue[Brick], fallen: Set[Brick] = Set.empty, ground:Map[(Int,Int), Int] = Map.empty): Set[Brick]=
+    if (floating.isEmpty)
+      fallen
+      else{
+        val lowestBrick = floating.dequeue()
+        val lowestPossible = lowestBrick.planarProjection.map(ground.getOrElse(_, 0)).max + 1
+        val fallenBrick = lowestBrick.fall(lowestBrick.lowestHeight - lowestPossible)
+
+        fall(floating, fallen + fallenBrick, ground ++ {
+          val highestHeight = fallenBrick.points.map(_._3).max
+          fallenBrick.planarProjection.map((_, highestHeight))
+        })
+//
+//
+//    bricks.find(brick => brick.lowestHeight > 1 && !bricks.exists(_.points overlaps brick.floorProjection)) match {
+//      case None => bricks
+//      case Some(brick) => fall(bricks - brick + Brick(brick.points.map {
+//        case (x, y, z) => (x, y, z - 1)
+//      }))
+//    }
   }
+
+  // 850 too high
 
   private implicit class SetsOverlap[T](val self:Set[T]) extends AnyVal {
 
--- src/test/scala/io/github/teonistor/adventofcode/y2023/_22Test.scala
+++ src/test/scala/io/github/teonistor/adventofcode/y2023/_22Test.scala
@@ -4,5 +4,5 @@ import io.github.teonistor.adventofcode.AdventOfCodeTestBase
 
 class _22Test extends AdventOfCodeTestBase {
 
-  autorun(_22, 5, -1, "1,0,1~1,2,1\n0,0,2~2,0,2\n0,2,3~2,2,3\n0,0,4~0,2,4\n2,0,5~2,2,5\n0,1,6~2,1,6\n1,1,8~1,1,9")
+  autorun(_22, 5, 7, "1,0,1~1,2,1\n0,0,2~2,0,2\n0,2,3~2,2,3\n0,0,4~0,2,4\n2,0,5~2,2,5\n0,1,6~2,1,6\n1,1,8~1,1,9")
 }
