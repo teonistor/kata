04666beda029cea2fe7098b30bd6d6eb41f402c0
Teodor G Nistor <teodor@nistor.uk>
2023-12-23 00:22:22 +0000

    AoC2023: Refactor day 22

--- src/main/scala/io/github/teonistor/adventofcode/y2023/_22.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2023/_22.scala
@@ -1,7 +1,9 @@
 package io.github.teonistor.adventofcode.y2023
 
+import com.google.common.annotations.VisibleForTesting
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
+import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.collection.mutable
 
@@ -10,104 +12,90 @@ object _22 extends StandardAdventOfCodeSolution[Int] {
   private val splitter = "(\\d+),(\\d+),(\\d+)~(\\d+),(\\d+),(\\d+)".r
 
   private case class Brick(points: Set[(Int,Int,Int)]) {
-    val lowestHeight = points.map(_._3).min
-    lazy val planarProjection = points.map { case (x,y,_) => (x,y) }
-    lazy val lowestProjection = points.map { case (x,y,_) => (x,y,lowestHeight)}
-    lazy val floorProjection = lowestProjection.map { case (x,y,z) => (x,y,z-1)}
+    private[_22] val lowestHeight = points.map(_._3).min
+    private[_22] lazy val planarProjection = points.map { case (x,y,_) => (x,y) }
+    private[_22] lazy val floorProjection = planarProjection.map { case (x,y) => (x,y,lowestHeight-1)}
 
-    def fall(howMuch: Int) = {
+    private[_22] def fall(howMuch: Int) =
       if (howMuch < 0)
-        println("Busted!")
-      if (howMuch > 0)
+        throw new IllegalArgumentException("Cannot fall up")
+      else if (howMuch > 0)
         Brick(points.map {
           case (x, y, z) => (x, y, z - howMuch)
         })
       else
         this
-    }
   }
 
   override def _1(input: String): Int = {
-
-val leanOn = computeLeaningMap(input)
-    val aye = leanOn.values.toSet
-      .filter(_.size == 1)
-
-
-    leanOn.size - aye.size
+    val downwards = computeLeaningMap(input)
+    downwards.size - downwards.values.toSet.count(_.size == 1)
   }
 
   override def _2(input: String): Int = {
-    val leanOn = computeLeaningMap(input)
-    val leanedOn = leanOn.iterator.flatMap {
-      case (above, below) => below.iterator.map((_, above))
-    }
-      .to(Set)
-      .groupMap(_._1)(_._2)
-//    val memo:mutable.Map[Brick,Int] = mutable.Map.empty
+    val downwards = computeLeaningMap(input)
+    val upwards = reverseDependence(downwards)
 
-    def ifItWereGone(disappearing:Queue[Brick], leanOnRemaining:Map[Brick,Set[Brick]]): Map[Brick, Set[Brick]] =
+    @tailrec
+    def ifItWereGone(disappearing:Queue[Brick], downwardsRemaining:Map[Brick,Set[Brick]]): Map[Brick, Set[Brick]] =
       if (disappearing.isEmpty)
-        leanOnRemaining
+        downwardsRemaining
+
       else {
-        val nextToGo = disappearing.head
-        val lll = leanedOn.getOrElse(nextToGo, Set.empty).iterator
-          .foldLeft(leanOnRemaining) ((lr, buf) => lr.updated(buf, lr(buf) - nextToGo))
-        ifItWereGone(disappearing.tail ++ leanedOn.getOrElse(nextToGo, Set.empty).filter(u => lll(u).isEmpty), lll)
+        val nextToDisappear = disappearing.head
+        val aboveTheDisappeared = upwards.getOrElse(nextToDisappear, Set.empty)
+        val nextDownwards = aboveTheDisappeared.iterator
+          // In other words: for every brick above the one about to be disappeared, we remove the one about to disappear from the downwards map
+          .foldLeft(downwardsRemaining)((downwardsBeingUpdated, oneAboveTheDisappeared) => downwardsBeingUpdated.updatedWith(oneAboveTheDisappeared)(_.map(_ - nextToDisappear)))
+        // then, all bricks with nothing left beneath them become candidates for disappearing (which is the same as falling for the purpose
+        // of this quest). Note we don't filter them from the whole world, just from aboveTheDisappeared, so as not to drag unrelated bricks
+        // in - in particular, bricks lying on the ground
+        ifItWereGone(disappearing.tail ++ aboveTheDisappeared.filter(nextDownwards(_).isEmpty), nextDownwards)
       }
 
-    leanOn.keysIterator
-      // This is also incorrect because bricks directly on the ground lean on nothing but wouldn't fall
-      .map(u => ifItWereGone(Queue(u), leanOn).count(U => U._1.lowestHeight > 1 && U._2.isEmpty))
+    downwards.keysIterator
+      .map(disappearanceCandidate => ifItWereGone(Queue(disappearanceCandidate), downwards).count {
+        case (possiblyFloating, beneath) => possiblyFloating.lowestHeight > 1 && beneath.isEmpty
+      })
       .sum
   }
 
   private def computeLeaningMap(input:String)={
-    // x,y,z follow the Blender convention
-    val bricks = input.split('\n').iterator.map {
-      case splitter(x1, y1, z1, x2, y2, z2) =>
-        Brick((x1.toInt to x2.toInt).flatMap(x =>
-          (y1.toInt to y2.toInt).flatMap(y =>
-            (z1.toInt to z2.toInt).map((x, y, _)))).toSet)
-    }.toSet
+    val settledBricks = fall(mutable.PriorityQueue
+      .newBuilder(Ordering.by(-(_: Brick).lowestHeight))
+      .addAll(input.split('\n').iterator.map {
+        // x,y,z follow the Blender convention
+        case splitter(x1, y1, z1, x2, y2, z2) =>
+          Brick((x1.toInt to x2.toInt).flatMap(x =>
+            (y1.toInt to y2.toInt).flatMap(y =>
+              (z1.toInt to z2.toInt).map((x, y, _)))).toSet)
+      })
+      .result())
 
-    val que = mutable.PriorityQueue.newBuilder(Ordering.by(-(_:Brick).lowestHeight))
-      .addAll(bricks)
-      .result()
-
-
-    val settledBricks = fall(que)
-    settledBricks.map(brick =>(brick, settledBricks.filter(_.points.exists(it => brick.floorProjection contains it))))
+    settledBricks.map(brick => (brick, settledBricks.filter(_.points.exists(brick.floorProjection.contains))))
       .toMap
   }
 
+  @tailrec
   private def fall(floating: mutable.PriorityQueue[Brick], fallen: Set[Brick] = Set.empty, ground:Map[(Int,Int), Int] = Map.empty): Set[Brick]=
     if (floating.isEmpty)
       fallen
-      else{
-        val lowestBrick = floating.dequeue()
-        val lowestPossible = lowestBrick.planarProjection.map(ground.getOrElse(_, 0)).max + 1
-        val fallenBrick = lowestBrick.fall(lowestBrick.lowestHeight - lowestPossible)
+    else {
+      val lowestBrick = floating.dequeue()
+      val lowestPossible = lowestBrick.planarProjection.map(ground.getOrElse(_, 0)).max + 1
+      val fallenBrick = lowestBrick.fall(lowestBrick.lowestHeight - lowestPossible)
 
-        fall(floating, fallen + fallenBrick, ground ++ {
-          val highestHeight = fallenBrick.points.map(_._3).max
-          fallenBrick.planarProjection.map((_, highestHeight))
-        })
-//
-//
-//    bricks.find(brick => brick.lowestHeight > 1 && !bricks.exists(_.points overlaps brick.floorProjection)) match {
-//      case None => bricks
-//      case Some(brick) => fall(bricks - brick + Brick(brick.points.map {
-//        case (x, y, z) => (x, y, z - 1)
-//      }))
-//    }
-  }
+      fall(floating, fallen + fallenBrick, ground ++ {
+        val highestHeight = fallenBrick.points.map(_._3).max
+        fallenBrick.planarProjection.map((_, highestHeight))
+      })
+    }
 
-  // 850 too high
-
-  private implicit class SetsOverlap[T](val self:Set[T]) extends AnyVal {
-
-    def overlaps(other:Set[T]) =
-      self.exists(other)
-  }
+  @VisibleForTesting
+  private[y2023] def reverseDependence[T](input: Map[T, Set[T]]) =
+    input.iterator.flatMap {
+        case (above, below) => below.iterator.map((_, above))
+      }
+      .to(Set)  // T shouldn't be erratic under equals()
+      .groupMap(_._1)(_._2)
 }
--- src/test/scala/io/github/teonistor/adventofcode/y2023/_22Test.scala
+++ src/test/scala/io/github/teonistor/adventofcode/y2023/_22Test.scala
@@ -6,18 +6,13 @@ class _22Test extends AdventOfCodeTestBase {
 
   autorun(_22, 5, 7, "1,0,1~1,2,1\n0,0,2~2,0,2\n0,2,3~2,2,3\n0,0,4~0,2,4\n2,0,5~2,2,5\n0,1,6~2,1,6\n1,1,8~1,1,9")
 
-  test("Stug") {
-    val leanOn = Map(
+  test("Spot-check reverseDependence()") {
+    assert(_22.reverseDependence(Map(
       "A" -> Set("B", "C"),
       "B" -> Set("D"),
-      "C" -> Set("D"),
-    "D" -> Set.empty)
-    val leanedOn = leanOn.iterator.flatMap {
-        case (above, below) => below.iterator.map((_, above))
-      }
-      .to(Set)
-      .groupMap(_._1)(_._2)
-
-    println(leanedOn)
+      "C" -> Set("D"))) == Map(
+      "B" -> Set("A"),
+      "C" -> Set("A"),
+      "D" -> Set("B", "C")))
   }
 }
