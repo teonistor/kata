84a6b84c8849567db55b05478d80c9c26ce4dec0
Teodor G Nistor <teodor@nistor.uk>
2024-12-02 12:05:59 +0000

    AoC2024 - 2

---
+++ src/test/scala/io/github/teonistor/adventofcode/y2024/_02Test.scala
@@ -0,0 +1,7 @@
+package io.github.teonistor.adventofcode.y2024
+
+import io.github.teonistor.adventofcode.AdventOfCodeTestBase
+
+class _02Test extends AdventOfCodeTestBase {
+  autorun(_02, 2, 4, "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9")
+}
