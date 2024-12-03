e97dc92a647e5006bfe2a6b191a206dc131acc14
Teodor G Nistor <teodor@nistor.uk>
2024-12-03 20:33:31 +0000

    AoC2024 - 3 - refactor

--- src/main/scala/io/github/teonistor/adventofcode/y2024/_03.scala
+++ src/main/scala/io/github/teonistor/adventofcode/y2024/_03.scala
@@ -2,34 +2,28 @@ package io.github.teonistor.adventofcode.y2024
 
 import io.github.teonistor.adventofcode.StandardAdventOfCodeSolution
 
+// noinspection NameBooleanParameters
 object _03 extends StandardAdventOfCodeSolution[Long] {
 
-  override def _1(input: String): Long = {
-    val finder = "mul\\((\\d+),(\\d+)\\)".r
-    finder.findAllMatchIn(input)
-      .map(m => List(m.group(1), m.group(2))
-      .map(_.toLong)
-      .product)
-      .sum
-  }
+  override def _1(input: String): Long =
+    solve(input, true)
 
-  override def _2(input: String): Long = {
-    val finder = "mul\\((\\d+),(\\d+)\\)|(do\\(\\))|(don't\\(\\))".r
-    finder.findAllMatchIn(input)
+  override def _2(input: String): Long =
+    solve(input, false)
+
+  private def solve(input: String, ignoreOnOff: Boolean) =
+    "mul\\((\\d+),(\\d+)\\)|(do\\(\\))|(don't\\(\\))".r
+      .findAllMatchIn(input)
       .map(m =>
         if (m.group(3) != null) Left(true)
-        else if (m.group(4) != null) {
-          println("bong")
-          Left(false)
-        }
-        else Right(List(m.group(1), m.group(2))
-          .map(_.toLong)
-          .product) )
-      .foldLeft((true, 0L)){
-        case ((_,sum),Left(enabled)) => (enabled,sum)
-        case ((true,sum), Right(next)) =>( true,sum + next)
+        else if (m.group(4) != null) Left(ignoreOnOff)
+        else Right(List(1, 2)
+          .map(m.group(_).toLong)
+          .product))
+      .foldLeft((true, 0L)) {
+        case ((_, sum), Left(enabled)) => (enabled, sum)
+        case ((true, sum), Right(next)) => (true, sum + next)
         case (otherwise, _) => otherwise
       }
       ._2
-  }
 }
